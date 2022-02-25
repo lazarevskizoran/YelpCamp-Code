93e5ad5ed5e196a7cd585fe4c43e7f03b62c85f2
lazarevskizoran <100445665+lazarevskizoran@users.noreply.github.com>
2022-02-25 23:35:52 +0100

    Adding Project

---
+++ routes/reviews.js
@@ -0,0 +1,42 @@
+const express = require('express');
+// we can use mergeParams or we can change the
+// routes so the id will be in the routes
+const router = express.Router({ mergeParams: true });
+
+const Campground = require('../models/campground');
+const Review = require('../models/review')
+
+const { reviewSchema } = require('../schemas.js')
+
+const ExpressError = require('../utils/ExpressError');
+const catchAsync = require('../utils/catchAsync');
+
+const validateReview = (req, res, next) => {
+    const { error } = reviewSchema.validate(req.body)
+    if (error) {
+        const msg = error.details.map(el => el.message).join(',')
+        throw new ExpressError(msg, 400)
+    } else {
+        next();
+    }
+}
+
+router.post('/:id/reviews', validateReview, catchAsync(async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    const review = new Review(req.body.review)
+    campground.reviews.push(review);
+    await review.save();
+    await campground.save();
+    req.flash('success', 'Created new review')
+    res.redirect(`/campgrounds/${campground._id}`);
+}))
+
+router.delete('/:id/reviews/:reviewId', catchAsync(async (req, res) => {
+    const { id, reviewId } = req.params;
+    await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
+    await Review.findByIdAndDelete(reviewId);
+    req.flash('success', 'Successfully deleted review')
+    res.redirect(`/campgrounds/${id}`)
+}))
+
+module.exports = router;
---
+++ seeds/index.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+const cities = require('./cities')
+const { places, descriptors } = require('./seedHelpers')
+const Campground = require('../models/campground')
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp')
+    .then(() => {
+        console.log("MONGO CONNECTION OPEN!!!")
+    })
+    .catch(err => {
+        console.log("Error, MONGO CONNECTION!!!!")
+        console.log(err)
+    })
+// This can Also work as Error handling
+// const db = mongoose.connection;
+// db.on('error', console.error.bind(console, "connection error:"));
+// db.once('open', () => {
+//     console.log("Database Connected");
+// });
+
+const sample = array => array[Math.floor(Math.random() * array.length)];
+
+const seedDB = async () => {
+    await Campground.deleteMany({});
+    for (let i = 0; i < 50; i++) {
+        const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 100);
+        const camp = new Campground({
+            location: `${cities[random1000].city}, ${cities[random1000].state}`,
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat velit iure ut qui consectetur corporis laborum ipsam maxime dolore voluptatum adipisci sequi vero quis nobis eaque inventore, alias veniam laboriosam.',
+            price
+        })
+        await camp.save();
+    }
+}
+
+seedDB().then(() => {
+    mongoose.connection.close()
+})
